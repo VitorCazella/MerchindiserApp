6b779ef5264c19e37d26eb9fc08d30eee2a5cbe7
Vitor A. Cazella <vitor.cazella@gmail.com>
2020-12-09 18:41:45 +1300

    Add project files.

---
+++ MerchindiserApp/MerchindiserApp/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MerchindiserApp.ViewModels;
+using MerchindiserApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MerchindiserApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ MerchindiserApp/MerchindiserApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MerchindiserApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MerchindiserApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
