b505650b03a45da19d22d8ee2b2337e457bd9fea
Nicole <65936364+StarNicBeep@users.noreply.github.com>
2020-12-10 09:07:55 +1300

    Nicole_Changes

--- MerchindiserApp/MerchindiserApp/Views/AboutPage.xaml.cs
+++ MerchindiserApp/MerchindiserApp/Views/AboutPage.xaml.cs
@@ -11,5 +11,22 @@ namespace MerchindiserApp.Views
         {
             InitializeComponent();
         }
+        async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ItemsPage());
+
+        }
+
+        async void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProfilePage());
+
+        }
+
+        async void ImageButton_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ClientPage());
+
+        }
     }
 }
